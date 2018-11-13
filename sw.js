5adbcc479532b120f1968772ee99fe2ca4191a49
aramoscoren <45004770+aramoscoren@users.noreply.github.com>
2018-11-13 14:00:15 -0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,14 @@
+//importScripts('/cache-polyfill.js');
+
 self.addEventListener('install', function(e) {
  e.waitUntil(
-   caches.open('video-store').then(function(cache) {
+   caches.open('coren-sp').then(function(cache) {
      return cache.addAll([
-       'https://aramoscoren.github.io/teste/',
-       'https://aramoscoren.github.io/teste/index.html',
-       'https://aramoscoren.github.io/teste/index.html/index.js'
+       '/',
+       '/index.html',
+       '/index.html?homescreen=1',
+       '/?homescreen=1'
      ]);
    })
  );
-});
-
-self.addEventListener('fetch', function(e) {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
 });
