19f66ffd90110918a84c74dea7801f4f9f1d2a08
aramoscoren <45004770+aramoscoren@users.noreply.github.com>
2018-11-13 14:12:39 -0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// Register service worker to control making site work offline
-
 if('serviceWorker' in navigator) {
   navigator.serviceWorker
            .register('https://aramoscoren.github.io/teste/sw.js')
