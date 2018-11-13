1b147910c23da26ca6283d2f6434c6841bcff163
aramoscoren <45004770+aramoscoren@users.noreply.github.com>
2018-11-13 14:11:19 -0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,20 @@
-importScripts('https://aramoscoren.github.io/teste/serviceworker-cache-polyfill.js');
-
 self.addEventListener('install', function(e) {
  e.waitUntil(
-   caches.open('coren-sp').then(function(cache) {
+   caches.open('video-store').then(function(cache) {
      return cache.addAll([
        'https://aramoscoren.github.io/teste/',
-       'https://aramoscoren.github.io/teste/index.html'
+       'https://aramoscoren.github.io/teste/index.html',
+       'https://aramoscoren.github.io/teste/index.js'
      ]);
    })
  );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
