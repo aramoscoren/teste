399fe11174955e7df5a0eb5bab533f62257b75ba
aramoscoren <45004770+aramoscoren@users.noreply.github.com>
2018-11-13 14:07:24 -0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,10 +4,8 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('coren-sp').then(function(cache) {
      return cache.addAll([
-       '/',
-       '/index.html',
-       '/index.html?homescreen=1',
-       '/?homescreen=1'
+       'https://aramoscoren.github.io/teste/',
+       'https://aramoscoren.github.io/teste/index.html'
      ]);
    })
  );
